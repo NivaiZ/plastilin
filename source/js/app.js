a89a061499d349344afc1d4f21b6e833af80576f
IlyaGalyatdinov <i.galyatdinov@yandex.ru>
2023-11-23 12:43:59 +0500

    почти доделал мобилку, поправил баги во многих местах

--- source/js/app.js
+++ source/js/app.js
@@ -3,6 +3,7 @@ import { teamSlider } from "./modules/teamSlider.js";
 import { chosenSlider } from "./modules/chosenSlider.js";
 import { maskPhone } from "./modules/iMask.js";
 import { validateForm } from "./modules/validateForm.js";
+import { useDynamicAdapt } from "./modules/dynamic-adapt.js";
 
 function handleDOMContentLoaded() {
   contentSlider();
@@ -10,6 +11,7 @@ function handleDOMContentLoaded() {
   chosenSlider();
   maskPhone();
   validateForm();
+  useDynamicAdapt();
 }
 
 document.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
--- source/js/modules/contentSlider.js
+++ source/js/modules/contentSlider.js
@@ -1,12 +1,27 @@
-export function contentSlider () {
+export function contentSlider() {
   const swiperElement = document.querySelector('.content__swiper.swiper');
 
-  if(swiperElement) {
+  if (swiperElement) {
     const swiper = new Swiper(swiperElement, {
-      slidesPerView: 3,
+      slidesPerView: 2,
+      navigation: {
+        nextEl: ".content__button.swiper-button-next",
+        prevEl: ".content__button.swiper-button-prev",
+      },
       pagination: {
-        el: ".swiper-pagination",
-        type: "progressbar",
+        el: '.swiper-pagination',
+        type: 'custom',
+        renderCustom: function (swiper, current, total) {
+          const progress = `<div class="swiper-pagination__progress" style="--pagination-progress: ${Math.floor((current / total) * 100)}%"></div>`
+
+          const fraction = `<div class="swiper-pagination__fraction">
+            <span class="swiper-pagination__fraction-current">${current}</span>
+            <span class="swiper-pagination__fraction-separator">/</span>
+            <span class="swiper-pagination__fraction-total">${total}</span>
+          </div>`
+
+          return progress + fraction;
+        }
       },
       breakpoints: {
         1440: {
